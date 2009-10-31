e7cc0ab7f6403a783bf96a84a4a3b2414abb0e3a
Robert Mao <zhihong.mao@gmail.com>
2009-10-31 23:18:14 +0000

    change form style, remove 'setting' page to make design simpler

--- apps/article/models.py
+++ apps/article/models.py
@@ -6,3 +6,14 @@ class Article(db.Model):
     title = db.StringProperty(required=True)
     content = db.TextProperty(required=True, )
     author = db.ReferenceProperty(User)
+    
+    # create time
+    ctime = db.DateTimeProperty()   
+    #last update time
+    mtime = db.DateTimeProperty()    
+    # language, for filtering and automatic translation support
+    lang = db.StringProperty(required=True, default='en')
+    
+    published = db.BooleanProperty(default=False, required=True)
+    
+        
