2ad4f7704a1f918effbb705429ea72e49dbe3c8f
Robert Mao <zhihong.mao@gmail.com>
2010-01-13 22:14:32 -0800

    more added to taggable app

--- apps/taggable/models.py
+++ apps/taggable/models.py
@@ -5,30 +5,56 @@ class Taggable(db.Model):
     tags = db.StringListProperty()
 
     @classmethod
+    def get_taggable(cls, obj):
+        try: 
+           return (obj.taggable_set)[0]
+        except:
+            return None
+    
+    @classmethod
     def get_by_tag(cls, tag):
        return Taggable.all().filter('tags =', tag) 
-
+   
+    @classmethod
+    def clear_tags(cls, obj):
+        '''clear_tags: remove all tag from an object'''
+        taggable = Taggable.get_taggable(obj)
+        if taggable is not None: 
+            taggable.delete()
+            
+    @classmethod 
+    def set_tags(cls, obj, tags):
+        taggable = Taggable.get_taggable(obj)
+        if taggable is None:
+            taggable = Taggable(target = obj, tags = tags)  
+        else:
+            taggable.tags = tags
+        taggable.put()
+        
     @classmethod
     def add_tags(cls, obj, tags):
-       try: 
-           taggable = (obj.taggable_set)[0]
-           taggable.tags = tags
-       except: 
-           taggable = Taggable(target = obj, tags = tags)
-       taggable.put()
+        taggable = Taggable.get_taggable(obj)
+        if taggable is None: 
+          taggable = Taggable(target = obj, tags = tags)           
+        else: 
+            for tag in tags:
+               if tag not in taggable.tags:
+                   taggable.tags.append(tag)
+        taggable.put()
        
     @classmethod
     def remove_tags(cls, obj, tags):
-        try:
-            taggable = (obj.taggable_set)[0]
-            taggable.tags = []
+        taggable = Taggable.get_taggable(obj)
+        if taggable is not None: 
+            for tag in tags: 
+                if tag in taggable.tags: 
+                    taggable.tags.remove(tag)
             
             if len(taggable.tags) == 0 : 
                 taggable.delete()
             else:
                 taggable.put()
-        except:
-            pass
+
    
 class Tag(db.Model):
     name = db.StringProperty(required = True)      
---
+++ apps/taggable/templatetags/tags.py
@@ -0,0 +1,27 @@
+import logging
+from django import template
+from django.template.loader import render_to_string
+from django.http import HttpRequest
+from taggable.models import Taggable
+
+register = template.Library()
+
+class ShowTagsNode(template.Node):
+    def __init__(self, obj_ref):
+        self.obj_ref = template.Variable(obj_ref)
+    
+    def render(self, context):
+        obj = self.obj_ref.resolve(context)
+        if obj is not None: 
+            taggable = Taggable.get_taggable(obj)        
+            if taggable is not None:
+                return render_to_string('taggable/tags.html', {'tags': taggable.tags})
+        return ''
+
+def do_show_tags(parser, token):
+    tokens = token.contents.split()
+    if len(tokens) != 2:
+        raise template.TemplateSyntaxError("%r tag usage: { show_tags  object}" % tokens[0])
+    return ShowTagsNode(tokens[1])
+
+register.tag('show_tags', do_show_tags)
