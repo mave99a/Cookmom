08d4154c2187c3448d0ceaa98e2fbf80212781e6
Robert Mao <zhihong.mao@gmail.com>
2010-01-23 00:11:13 -0800

    remove focus dotted frame

--- apps/taggable/media/js/tags.js
+++ apps/taggable/media/js/tags.js
@@ -4,12 +4,12 @@
 $(document).ready(function() {
   if (typeof(_JS_MODULE_TAG) != 'undefined') {
   	$('a.tag span.remove').click(function() {
-  				$(this).parent().addClass('deletedtag').removeClass('tag');
+  				$(this).parent().addClass('deletedtag').removeClass('tag').blur();
   				$.get($(this).attr('href'));
 				return false; // cancel event 
 			})
    	$('a.tag span.undo').click(function() {
-  				$(this).parent().addClass('tag').removeClass('deletedtag');
+  				$(this).parent().addClass('tag').removeClass('deletedtag').blur();
   				$.get($(this).attr('href'));
 				return false; // cancel event 
 			}) 					   
