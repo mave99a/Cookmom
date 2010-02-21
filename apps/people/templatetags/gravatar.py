335ff8b3fd08e5c86f68379a6b76b82c1d0bfb7a
Robert Mao <zhihong.mao@gmail.com>
2010-02-21 14:52:19 -0800

    improve gavater to support identicon

--- apps/people/templatetags/gravatar.py
+++ apps/people/templatetags/gravatar.py
@@ -1,3 +1,9 @@
+'''
+    Gravatar template tag
+    
+    The Gravatar URL rule is defined here: 
+    http://en.gravatar.com/site/implement/url
+'''
 from django import template
 from django.utils.html import escape
 from django.contrib.auth.models import User
@@ -6,10 +12,11 @@ import urllib, hashlib
 
 register = template.Library()
 
-def gravatar(email, size=80, default_img = None):
-        
-    gravatar_url = "http://www.gravatar.com/avatar.php?"
-    gravatar_url += urllib.urlencode({'gravatar_id':hashlib.md5(email).hexdigest(), 'default': default_img, 'size':str(size)})
+def gravatar(email, size=80, default_img = None):   
+    gravatar_url = "http://www.gravatar.com/avatar/%s?" % hashlib.md5(email).hexdigest()
+    if default_img is None: 
+       default_img = 'identicon' 
+    gravatar_url += urllib.urlencode({'d': default_img, 's':str(size)})
     return gravatar_url
 
 register.simple_tag(gravatar)
