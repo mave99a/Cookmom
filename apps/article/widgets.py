85b1cc94c8c61864ba0199c7928f1c1bf165d373
Robert Mao <zhihong.mao@gmail.com>
2010-02-24 01:54:30 -0800

    adding management templates

--- apps/article/widgets.py
+++ apps/article/widgets.py
@@ -1,3 +1,8 @@
 from models import Article
-def test():
-    return Article.all()
+def manage_draft(request):
+    drafts = request.current_user.article_set.filter('published =', False)
+    return drafts
+
+def manage_published(request):
+    drafts = request.current_user.article_set.filter('published =', True)
+    return drafts
