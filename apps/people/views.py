c5ffc210f71d9071d473b3fd206bd0fd06d0de50
Robert Mao <zhihong.mao@gmail.com>
2010-02-26 01:51:31 -0800

    finish up the sidebar design and changed styles

--- apps/people/views.py
+++ apps/people/views.py
@@ -7,6 +7,7 @@ from django.views.generic.create_update import delete_object
 from generic_view_patch.create_update import update_object
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 from models import User
 from auth.decorators import login_required
 
@@ -20,17 +21,20 @@ def list_user(request):
     return object_list(request, User.all(), paginate_by=10)
 
 def show_user(request, id):
-    return object_detail(request, User.all(), object_id=id )
+    return show_user_detail(request, id)
 
-def show_user_detail(request, id, obj):
-    template='people/user_detail_' + obj + '.html'
-    return object_detail(request, User.all(), object_id=id, template_name=template)
+def show_user_detail(request, id, obj=None):
+    is_self = (int(id) == request.current_user.id())
+    if obj is not None: 
+        template='people/user_detail_' + obj + '.html'
+    else:
+        template = None;
+    return object_detail(request, User.all(), object_id=id, template_name=template, extra_context={'is_self': is_self})
 
 @login_required
 def show_myhome(request):
-    return render_to_response('people/my.html',
-                              {'object': request.current_user},
-                              context_instance=RequestContext(request))
+    return HttpResponseRedirect(reverse(show_user, args =[request.current_user.id()]))
+
 @login_required
 def edit(request):
     return update_object(request, object_id=request.current_user.id(), form_class=UserForm)
