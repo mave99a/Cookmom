592e8f58c5fd7d6c1e1173badca269c550870d9e
Robert Mao <zhihong.mao@gmail.com>
2010-02-27 13:24:41 -0800

    improve the list styles

---
+++ apps/people/settings.py
@@ -0,0 +1,10 @@
+from ragendja.settings_post import settings
+
+settings.add_uncombined_app_media('photostoryeditor')
+#
+settings.add_app_media('combined-%(LANGUAGE_DIR)s.css',
+    'people/css/people.css'
+)
+
+#settings.add_app_media('combined-%(LANGUAGE_CODE)s.js',
+#)
