4e55571ffda7596b1da56d34b5b79a55e578b063
Robert Mao <zhihong.mao@gmail.com>
2010-03-04 22:37:11 -0800

    fixed the XFBML re-rendering during ajax call

--- apps/comment/media/js/comment.js
+++ apps/comment/media/js/comment.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
 	$('form.comment').ajaxForm({
             resetForm: true,
 			success: function(data) {
-                $('.comments_list table').prepend(data)
+                newadd = $('.comments_list table').prepend(data)
+                
+                if (typeof(FB) != 'undefined') {
+                    // make sure newly added FBML get displayed correctly
+                    FB.XFBML.Host.parseDomElement(newadd.find('td.author').get(0));
+                }
 			}
 		}
 	)
