da7864a18cfc11819f6e0b0493a0eb43e3f89c1b
Robert Mao <zhihong.mao@gmail.com>
2010-03-24 22:29:56 -0700

    move taggable app out

--- apps/taggable/settings.py
+++
@@ -1,9 +0,0 @@
-from ragendja.settings_post import settings
-
-settings.add_app_media('combined-%(LANGUAGE_DIR)s.css',
-    'taggable/css/tags.css'
-)
-
-settings.add_app_media('combined-%(LANGUAGE_CODE)s.js',
-    'taggable/js/tags.js',
-)
