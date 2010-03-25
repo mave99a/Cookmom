46ac6fd09b28cc862e8319cd0e39442221999a51
Robert Mao <zhihong.mao@gmail.com>
2010-03-24 22:14:28 -0700

    move reusable app 'auth' out

--- apps/auth/middleware.py
+++
@@ -1,6 +0,0 @@
-from user import get_current_user
-
-class AuthMiddleware(object):
-    def process_request(self, request):
-        request.authuser = get_current_user(request)
-        request.current_user = request.authuser.user
