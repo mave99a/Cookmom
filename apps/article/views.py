0003ae52f88bd3c88eb63ae6524152418fd2d2dd
Robert Mao <zhihong.mao@gmail.com>
2010-03-28 17:52:05 -0700

    Initial using GAEGenericViews

--- apps/article/views.py
+++ apps/article/views.py
@@ -4,46 +4,13 @@ from django.http import HttpResponse, Http404
 from django.views.generic.create_update import delete_object
 from generic_view_patch.create_update import create_object, update_object
 from renderhelpers.renderblock import direct_block_to_template
-from renderhelpers.utils import AutoRendResponse
+
 from renderhelpers.decorators import AutoResponse
 from models import Article, ArticleForm
 from auth.decorators import login_required
 from people.models import User
+from GAEGenericViews.genericviews import *
 
-from django.core.paginator  import Paginator, InvalidPage
-class GenericViews():
-
-    @classmethod
-    def object_list(cls, request, queryset = None):        
-        paginator = Paginator(queryset, 3,  allow_empty_first_page=True)
-        page = request.GET.get('page', 1)
-            
-        try:
-            page_number = int(page)
-        except ValueError:
-            if page == 'last':
-                page_number = paginator.num_pages
-            else:
-                # Page is not 'last', nor can it be converted to an int.
-                page_number = 1
-        try:
-            page_obj = paginator.page(page_number)
-        except InvalidPage:
-            page_obj = paginator.page(1)
-    
-        object_list = page_obj.object_list
-        
-        return AutoRendResponse(request, template='article/article_list.html', autoAjax=False, redirectBack=False, context=locals())
-
-
-    @classmethod
-    def detail(cls, request, id, title=None):
-        object = cls.model.get_by_id(int(id))
-        if object is None: 
-            raise Http404('%s Not found.' % id) 
-      
-        return AutoRendResponse(request, template='article/article_detail.html', autoAjax=False, redirectBack=False, context=locals())
-        
 class ArticleViews(GenericViews):
     model = Article
     
